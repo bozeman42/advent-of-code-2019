ea77fc2b8d87907e198b56733c66e3e9409a7256
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2019-12-14 01:21:01 -0600

    solved day 2 question 2

--- 02/index.js
+++ 02/index.js
@@ -10,3 +10,18 @@ const result = utils.intCode(newData)
 console.timeEnd('D2Q1')
 console.log(`Day 2, Q1: ${result}`)
 
+function testNounsAndVerbs (target) {
+  let verb = 0
+  let noun = 0
+  let value = 0
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100 && value < target; verb++) {
+      let testData = utils.setNounVerb(noun, verb, data)
+      value = utils.intCode(testData)
+      if (value === target) return 100 * noun + verb
+    }
+  }
+}
+
+let nounVerbResult = testNounsAndVerbs(19690720)
+console.log(nounVerbResult)
