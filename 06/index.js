1e5debc4e25c21443b2c55861918d564928f6c22
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2019-12-14 13:16:19 -0600

    Day 6 Question 2 complete

--- 06/index.js
+++ 06/index.js
@@ -16,15 +16,29 @@ const data = utils.readFile('./06/input.txt')
     }
   }, {})
   let count = 0
-  Object.values(data)
+  let youPath = []
+  let santaPath = []
+  Object.keys(data)
   .forEach(orbit => {
-    planet = orbit
+    planet = data[orbit]
     count++
     while (planet !== 'COM') {
       planet = data[planet]
+      if (orbit === 'YOU') {
+        youPath.push(planet)
+      } else if (orbit === 'SAN') {
+        santaPath.push(planet)
+      }
       count++
     }
   })
-
-
 console.log(count)
+
+const distance = youPath.reduce((distance, planet, youIndex) => {
+  const santaIndex = santaPath.indexOf(planet)
+  if (distance !== -1) return distance
+  if (santaIndex !== -1) return youIndex + santaIndex + 2
+  return distance
+}, -1)
+
+console.log(distance)
